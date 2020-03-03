1b488f773d1a239c799a7d850b8671a1079fe68f
Chris De Gour <cdegour@cdegour-ltm01.internal.salesforce.com>
2020-03-03 10:13:29 -0800

    initial push so i can switch scratch orgs

---
+++ force-app/main/default/lwc/contactBigRideRelatedList/contactBigRideRelatedList.js
@@ -0,0 +1,32 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import getCustomerRecentRideRecords from '@salesforce/apex/bigRideUtils.getCustomerRecentRideRecords';
+
+const rideColumns = [
+    {label: 'Customer', fieldName: 'Contact__c'},
+    {label: 'Start Time', fieldName: 'Start_Time__c', type: 'date-local'},
+    {label: 'End Time', fieldName: 'End_Time__c', type: 'date-local'},
+    {label: 'Rental Unit ID', fieldName: 'Rental_Unit__c'},
+    //{label: 'Start Latitude', fieldName: 'Start_Location_Lat__c'},
+    //{label: 'Start Longitude', fieldName: 'Start_Location_Long__c'},
+    //{label: 'End Latitude', fieldName: 'End_Location_Lat__c'},
+    //{label: 'End Longitude', fieldName: 'End_Location_Long__c'},
+    {label: 'Trip ID', fieldName: 'Trip_ID__c'}
+];
+
+export default class ContactBigRideRelatedList extends LightningElement {
+    @api recordId;
+    @track rideRecords;
+    @track error;
+    @track rideColumns = rideColumns;
+
+    @wire(getCustomerRecentRideRecords, {contactID: '$recordId'})
+    handleRideRecords(data, error){
+        if(data){
+            this.rideRecords = data;
+            this.error = undefined;
+        }else if(error){
+            this.rideRecords = undefined;
+            this.error = error;
+        }
+    }
+}
